18432172c65707ab1dca0c187cd45609a29c7fc2
vonklauz <russke46@gmail.com>
2025-05-07 14:12:11 +0300

    чистка мусора

--- app/Components/Header/DeviceHeader.tsx
+++
@@ -1,7 +0,0 @@
-export const DeviceHeader = () => {
-    return (
-        <div>
-            <h1>DeviceHeader</h1>
-        </div>
-    )
-}
--- app/Components/Header/HeaderSkeleton.tsx
+++
@@ -1,7 +0,0 @@
-import styles from './Header.module.css';
-
-export const HeaderSkeleton = () => (
-    <header className={`${styles.header} ${styles.headerSkeleton}`}>
-
-    </header>
-)
