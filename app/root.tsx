13e398214f29db70411872d41611e221539d93bc
vonklauz <russke46@gmail.com>
2025-07-16 11:25:06 +0300

    добавил новые стили в лендинг

--- app/root.tsx
+++ app/root.tsx
@@ -36,7 +36,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
           <Meta />
           <Links />
         </head>
-        <body className="antialiased">
+        <body className="antialiased flex flex-col justify-between min-h-dvh">
           {children}
           <ScrollRestoration />
           <Scripts />
